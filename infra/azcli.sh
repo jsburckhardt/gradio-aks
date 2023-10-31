3da4de36777b58159a9ef0f0dc02898de64c89f5
Juan <jsburckhardt@gmail.com>
2023-11-01 08:59:25 +1100

    feat(scripts): update the helper scripts to validate if resource exists (#10)
    feat(scripts): update the helper scripts to validate if resource exists (#10)

--- infra/azcli.sh
+++ infra/azcli.sh
@@ -6,21 +6,21 @@ LOCATION="australiaeast"
 SERVICE_ACCOUNT_NAMESPACE="default"
 SERVICE_ACCOUNT_NAME="gradio-aks-sa"
 CLUSTER_NAME="aks-swiftsolve-01"
-ACR="amldsworkspaceacr"
+USER_ASSIGNED_IDENTITY_NAME="gradioaksswiftsolveidentity"
+FEDERATED_IDENTITY_CREDENTIAL_NAME="gradioaksswiftsolveidentity-federated-credential"
 
 # Create AKS cluster
-az aks create -g "${RESOURCE_GROUP}" -n "${CLUSTER_NAME}" --node-count 1 --enable-oidc-issuer --enable-workload-identity --attach-acr "${ACR}" --enable-managed-identity || exit 1
+az aks show -g "${RESOURCE_GROUP}" -n "${CLUSTER_NAME}" || az aks create -g "${RESOURCE_GROUP}" -n "${CLUSTER_NAME}" --node-count 1 --enable-oidc-issuer --enable-workload-identity --enable-managed-identity || exit 1
 
 # Get OIDC issuer URL
 AKS_OIDC_ISSUER="$(az aks show -n "${CLUSTER_NAME}" -g "${RESOURCE_GROUP}" --query "oidcIssuerProfile.issuerUrl" -otsv)" || exit 1
 
 # Create user-assigned identity
-USER_ASSIGNED_IDENTITY_NAME="gradioaksidentity"
-az identity create --name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" --location "${LOCATION}" || exit 1
+az identity show --name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" || az identity create --name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" --location "${LOCATION}" || exit 1
 
 # Assign role to user-assigned identity
 RESOURCE_ID="$(az group show -g "${RESOURCE_GROUP}" --query "id" -o tsv)" || exit 1
-az role assignment create --role "Cognitive Services User" --assignee "$(az identity show --resource-group "${RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'clientId' -otsv)" --scope "${RESOURCE_ID}" || exit 1
+ az identity show --resource-group "${RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'clientId' -otsv || az role assignment create --role "Cognitive Services User" --assignee "$(az identity show --resource-group "${RESOURCE_GROUP}" --name "${USER_ASSIGNED_IDENTITY_NAME}" --query 'clientId' -otsv)" --scope "${RESOURCE_ID}" || exit 1
 
 # Retrieve aks credentials
 az aks get-credentials -g "${RESOURCE_GROUP}" -n "${CLUSTER_NAME}" -a || exit 1
@@ -37,5 +37,4 @@ metadata:
 EOF
 
 # Federate the account and the managed identity
-FEDERATED_IDENTITY_CREDENTIAL_NAME="gradioaksidentity-federated-credential"
-az identity federated-credential create --name "${FEDERATED_IDENTITY_CREDENTIAL_NAME}" --identity-name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" --issuer "${AKS_OIDC_ISSUER}" --subject "system:serviceaccount:${SERVICE_ACCOUNT_NAMESPACE}:${SERVICE_ACCOUNT_NAME}" || exit 1
+az identity federated-credential show  --name "${FEDERATED_IDENTITY_CREDENTIAL_NAME}" --identity-name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" || az identity federated-credential create --name "${FEDERATED_IDENTITY_CREDENTIAL_NAME}" --identity-name "${USER_ASSIGNED_IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" --issuer "${AKS_OIDC_ISSUER}" --subject "system:serviceaccount:${SERVICE_ACCOUNT_NAMESPACE}:${SERVICE_ACCOUNT_NAME}" || exit 1
